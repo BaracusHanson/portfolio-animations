47364819fd932fa35a3c04111ce05f4294671718
Junior guinier <guinier95.js@gmail.com>
2023-11-05 12:25:47 +0100

    suivreTete , suivreCurseur ok

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,7 +1,9 @@
 import { OrbitControls } from "@react-three/drei";
 import { Avatar } from "./Avatar";
+import { useControls } from "leva";
 
 export const Experience = () => {
+  const { animation } = useControls({});
   return (
     <>
       <OrbitControls />
